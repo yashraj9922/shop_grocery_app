ae956ad03e4c3cbf7181805be01092e5b8fd7529
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-10-27 02:24:02 +0530

    adding dissmisible func and fallback content

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -47,8 +47,42 @@ class _GroceryListState extends State<GroceryList> {
     });
   }
 
+  void removeItem(GroceryItem item) {
+    setState(() {
+      _groceryItems.remove(item);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget content = const Center(
+      child: Text("No Items added yet..."),
+    );
+
+    if (_groceryItems.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: _groceryItems.length,
+        // itemCount: groceryItems.length,//dummy data
+        itemBuilder: (ctx, index) => Dismissible(
+          onDismissed: (direction) {
+            removeItem(_groceryItems[index]);
+          },
+          key: ValueKey(_groceryItems[index].id),
+          child: ListTile(
+            title: Text(_groceryItems[index].name),
+            leading: Container(
+              width: 24,
+              height: 24,
+              color: _groceryItems[index].category.color,
+            ),
+            trailing: Text(
+              _groceryItems[index].quantity.toString(),
+            ),
+          ),
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Your Groceries"),
@@ -65,21 +99,22 @@ class _GroceryListState extends State<GroceryList> {
           ),
         ],
       ),
-      body: ListView.builder(
-        itemCount: _groceryItems.length,
-        // itemCount: groceryItems.length,//dummy data
-        itemBuilder: (ctx, index) => ListTile(
-          title: Text(_groceryItems[index].name),
-          leading: Container(
-            width: 24,
-            height: 24,
-            color: _groceryItems[index].category.color,
-          ),
-          trailing: Text(
-            _groceryItems[index].quantity.toString(),
-          ),
-        ),
-      ),
+      // body: ListView.builder(
+      //   itemCount: _groceryItems.length,
+      //   // itemCount: groceryItems.length,//dummy data
+      //   itemBuilder: (ctx, index) => ListTile(
+      //     title: Text(_groceryItems[index].name),
+      //     leading: Container(
+      //       width: 24,
+      //       height: 24,
+      //       color: _groceryItems[index].category.color,
+      //     ),
+      //     trailing: Text(
+      //       _groceryItems[index].quantity.toString(),
+      //     ),
+      //   ),
+      // ),
+      body: content,
     );
   }
 }
